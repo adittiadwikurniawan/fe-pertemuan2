4845d6e2eff839e21826ed23e956183536526a68
adittiadwikurniawan <adittiadwikurniawan@gmail.com>
2026-09-06 13:38:33 +0700

    bismillah jago ngoding

--- app/task/[id]/page.tsx
+++ app/task/[id]/page.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
-import { getTodoDetail } from '@/lib/todos';
+import TodoStateOnlyApp from './components/TodoStateOnlyApp';
+import { getTodoDetail, getTodos } from '@/lib/todos';
 import TaskNotFound from './components/TaskNotFound';
 import TaskDetailCard from './components/TaskDetailCard';
+export default async function TodoPage() {
+
+  const initialTodos = await getTodos();
+  return (
+    <main className=" min-h-screen p-6 md:p-10 bg-white text-dark-70">
+      <div className=" max-w-2x1 mx-auto space-y-6"> 
+      <div className= " bg-white p-6 md:p-8 rounded 2x1 shadow-x1 border border-gray-70">
+      <header className= "mb-6 border-b border-gray-100 pb-4">
+        <h1 className= "text 2x1 md:text-3xl font-bold text-dark-70 text-center">
+          Daftar Tugas (Todo List)</h1> 
+      </header>
+
+      </div>
+      </div>
+    </main>
+  )
+}
 
 type DetailPageProps = {
   params: Promise<{
@@ -9,7 +27,7 @@ type DetailPageProps = {
   }>;
 };
 
-export default async function TodoDetailPage({ params }: DetailPageProps) {
+export default async function TodoPage({ params }: DetailPageProps) {
   const { id } = await params;
   const todo = await getTodoDetail(id);
 
